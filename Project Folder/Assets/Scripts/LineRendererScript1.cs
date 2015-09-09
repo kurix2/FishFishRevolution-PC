28b7992a5679c5325b9045de45e44456b3aa46da
Chris Lamb <clamb87@gmail.com>
2015-09-10 02:01:12 +0900

    Line now renders from fish's mouth to rod when reeling up

--- Project Folder/Assets/Scripts/LineRendererScript1.cs
+++ Project Folder/Assets/Scripts/LineRendererScript1.cs
@@ -3,23 +3,36 @@ using System.Collections;
 
 public class LineRendererScript1 : MonoBehaviour {
 
-	private GameObject start;
-	private GameObject end;
+	public string status;
+	public GameObject start;
+	public GameObject end;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		status = GameObject.Find("NetworkView_TestObject(Clone)").GetComponent<RodStatus>().getStatus();
+
 		if(!GameObject.Find("Bobber"))return;
 		else{
-		GameObject start = GameObject.Find("BobLauncher");
-		GameObject end = GameObject.Find("Bobber");
-		LineRenderer lineY = GameObject.Find ("LineRendererObject").GetComponent<LineRenderer> ();
+			GameObject start = GameObject.Find("BobLauncher");
+			LineRenderer lineY = GameObject.Find ("LineRendererObject").GetComponent<LineRenderer> ();
+			lineY.SetPosition(0, start.transform.position);
 
-		lineY.SetPosition(0, start.transform.position);
-		lineY.SetPosition(1, end.transform.position);
+			//If not reeling up, set the line end to the bobber. If reeling up, set the line end to the fish's mouth.
+			if(status != "reeling up"){
+				GameObject end = GameObject.Find("Bobber");
+				lineY.SetPosition(1, end.transform.position);
+			}
+			else if (status == "reeling up"){
+				GameObject end = GameObject.Find("MouthPoint");
+				lineY.SetPosition(1, end.transform.position);	
+			}
+
 		}
 	}
 }
